815439e668d3eff5427bba6bbba143e9bf29e7ed
MCCLab4804WD18 <olivierek@gmail.com>
2024-11-06 14:47:00 -0800

    Olivers good code that kyl;e didnt help with cause he SUCKS!

--- src/main/java/cs112/lab09/controllers/CityController.java
+++ src/main/java/cs112/lab09/controllers/CityController.java
@@ -1,4 +1,19 @@
 package cs112.lab09.controllers;
 
+import cs112.lab09.Constants;
+import cs112.lab09.RedSummer;
+import cs112.lab09.models.RevisedHistoricalEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static cs112.lab09.Constants.*;
+
 public class CityController {
+    public void initData(Constants.Event eventIndex){
+        String[] data = HISTORICAL_DATA[eventIndex.ordinal()];
+        RevisedHistoricalEvent event = new RevisedHistoricalEvent(data);
+    }
 }
--- src/main/java/cs112/lab09/controllers/MapController.java
+++ src/main/java/cs112/lab09/controllers/MapController.java
@@ -1,4 +1,47 @@
 package cs112.lab09.controllers;
 
+import cs112.lab09.RedSummer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static cs112.lab09.Constants.*;
+import static cs112.lab09.Constants.REDSUMMER_IMAGE_PATH;
+
 public class MapController {
+    @FXML
+    ImageView imageLabel;
+
+    public void initialize(){
+        Image image = new Image(REDSUMMER_IMAGE_PATH);
+        imageLabel.setImage(image);
+    }
+
+    public void handleSanFranciscoCAButton()throws IOException {
+
+//        Stage stage = (Stage)titleLabel.getScene().getWindow();
+//        FXMLLoader fxmlLoader = new FXMLLoader(RedSummer.class.getResource(CITY_VIEW_RESOURCE));
+//        Scene scene = new Scene(fxmlLoader.load());
+//        stage.setScene(scene);
+//        stage.setTitle(CITY_VIEW_TITLE);
+//        stage.show();
+    }
+
+    public void handleBisbeeAZButton()throws IOException {
+
+//        Stage stage = (Stage)titleLabel.getScene().getWindow();
+//        FXMLLoader fxmlLoader = new FXMLLoader(RedSummer.class.getResource(CITY_VIEW_RESOURCE));
+//        Scene scene = new Scene(fxmlLoader.load());
+//        stage.setScene(scene);
+//        stage.setTitle(CITY_VIEW_TITLE);
+//        stage.show();
+    }
 }
